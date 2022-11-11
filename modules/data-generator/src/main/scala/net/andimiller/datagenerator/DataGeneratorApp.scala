3d1ecd91d43a8782fa590b0d43c9fc6a2950fe87
Andi Miller <andi@andimiller.net>
2022-11-11 18:42:09 +0000

    remove unused stuff

--- modules/data-generator/src/main/scala/net/andimiller/datagenerator/DataGeneratorApp.scala
+++ modules/data-generator/src/main/scala/net/andimiller/datagenerator/DataGeneratorApp.scala
@@ -1,7 +1,6 @@
 package net.andimiller.datagenerator
 
 import cats.Monad
-import cats.implicits._
 import cats.effect.kernel.Async
 import cats.effect.std.Random
 import cats.effect.{ExitCode, IO, IOApp}
@@ -16,14 +15,14 @@ import scala.jdk.CollectionConverters.MapHasAsScala
 
 class DataGeneratorApp[T: Arbitrary: Encoder](programName: String) extends IOApp {
 
-  def items[F[_]: Monad: Async: Random]: Stream[F, String] =
+  def items[F[_]: Async: Random]: Stream[F, String] =
     for {
       seed <- Stream.eval(Random[F].nextLong)
       item <- Stream.eval(Async[F].delay { implicitly[Arbitrary[T]].arbitrary.pureApply(Gen.Parameters.default, Seed(seed)) })
       str   = item.asJson.noSpaces
     } yield str
 
-  def generate[F[_]: Monad: Async: Random](n: Long): F[Unit] =
+  def generate[F[_]: Async: Random](n: Long): F[Unit] =
     items[F]
       .take(n)
       .through(fs2.text.utf8.encode)
@@ -31,7 +30,7 @@ class DataGeneratorApp[T: Arbitrary: Encoder](programName: String) extends IOApp
       .compile
       .drain
 
-  def stream[F[_]: Monad: Async: Random](every: FiniteDuration): F[Unit] =
+  def stream[F[_]: Async: Random](every: FiniteDuration): F[Unit] =
     Stream
       .awakeEvery(every)
       .zip(items[F])
